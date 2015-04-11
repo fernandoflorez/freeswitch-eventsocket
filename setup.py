b9231e10ed1423a9f97ee866befd575b259f3cdd
Fernando Flórez <fernando@funciton.com>
2015-04-11 16:57:11 -0500

    modified setup.py for pypi registration

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(
-    name='eventsocket',
+    name='freeswitch-eventsocket',
     version=version,
     py_modules=['eventsocket'],
     author=u'Fernando Fl\xf3rez',
@@ -15,17 +15,5 @@ setup(
     license='http://www.apache.org/licenses/LICENSE-2.0',
     description='This is a work in progress abstraction class to handle '
     'freeswitch\'s eventsocket command lines.',
-    long_description=long_description,
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ]
+    long_description=long_description
 )
